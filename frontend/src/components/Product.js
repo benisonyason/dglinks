660e69c4a0ded823d429d6f83b76afb64de73abf
benisonyason <83278793+benisonyason@users.noreply.github.com>
2022-03-16 14:34:17 +0100

    Yes

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Rating from './Rating';
+import { Stack, CardGroup, Card } from "react-bootstrap";
 
 export default function Product(props) {
   const { product } = props;
   return (
-    <div key={product._id} className="card">
-      <Link to={`/product/${product._id}`}>
-        <img className="medium" src={product.image} alt={product.name} />
-      </Link>
-      <div className="card-body">
-        <Link to={`/product/${product._id}`}>
-          <h2>{product.name}</h2>
-        </Link>
-        <Rating
-          rating={product.rating}
-          numReviews={product.numReviews}
-        ></Rating>
-        <div className="row">
-          <div className="price">${product.price}</div>
-          <div>
-            <Link to={`/seller/${product.seller._id}`}>
-              {product.seller.seller.name}
+    <Stack gap={2} className="col-md-5 mx-auto">
+      <CardGroup>
+        <Card bg="success" border="Primary" >
+        <div key={product._id} className="card">
+          <Link to={`/product/${product._id}`}>
+            <Card.Img className="medium" src={product.image} alt={product.name} variant="top" style={{ height: '100%', width: '100%'}}/>
+          </Link>
+          <Card.Footer>
+            <Card.Title>
+            <Link to={`/product/${product._id}`}>
+              <h2>{product.name}</h2>
             </Link>
-          </div>
+            </Card.Title>
+            <Rating
+              rating={product.rating}
+              numReviews={product.numReviews}
+            ></Rating>
+            <div className="row">
+              <div className="price">${product.price}</div>
+              <div>
+                <Link to={`/seller/${product.seller._id}`}>
+                  {product.seller.seller.name}
+                </Link>
+              </div>
+            </div>
+          </Card.Footer>
         </div>
-      </div>
-    </div>
+        </Card>
+      </CardGroup>
+    </Stack>
   );
 }
